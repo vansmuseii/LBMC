2831d361fa78d3a9f97927186d4f76932638733b
vansmuseii <vansmuseii@gmail.com>
2016-05-02 18:09:35 -0700

    Added Vector3Float

---
+++ Cubes/src/cubes/Vector3Float.java
@@ -0,0 +1,30 @@
+/**
+ * *********************************************************************
+ *      file: FPVCamera.java
+ *      author: Frank Lapena, Chun Ho, Van Muse
+ *      class: CS 445 - Computer Graphics
+ *
+ *      assignment: Program 2
+ *      date last modified: 05/04/2016
+ *
+ *      purpose: This program is to demonstrate how to use the LWGJL
+ *              to move a cube.
+ *
+ **********************************************************************
+ */
+package cubes;
+
+/**
+ *
+ * @author VANSKEES
+ */
+public class Vector3Float {
+
+    public float x, y, z;
+
+    public Vector3Float(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
