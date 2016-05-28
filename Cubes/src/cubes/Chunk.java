78d059d23e146ff8e8978654be346a083b48f31a
proki <proki@134.71.110.210>
2016-05-27 18:55:51 -0700

    Used correct water texture better way to add sand

--- Cubes/src/cubes/Chunk.java
+++ Cubes/src/cubes/Chunk.java
@@ -24,7 +24,7 @@ import org.newdawn.slick.util.ResourceLoader;
 
 public class Chunk {
 
-    static final int CHUNK_SIZE = 32;
+    static final int CHUNK_SIZE = 30;
     static final int CUBE_LENGTH = 2;
     private Block[][][] Blocks;
     private int VBOVertexHandle;
@@ -58,61 +58,20 @@ public class Chunk {
         FloatBuffer VertexTextureData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
         for (float x = 0; x < CHUNK_SIZE; x++) {
             for (float z = 0; z < CHUNK_SIZE; z++) {
-                h = Math.abs(StartY + (int) (150 * noise.getNoise((int) x, (int) z)) * CUBE_LENGTH);
+                h = Math.abs(StartY + (int) (100 * noise.getNoise((int) x, (int) z)) * CUBE_LENGTH);
+
                 for (float y = 0; y <= h; y++) {
-
-                    //Generate River
-                    if (y == 0) {
-                        Blocks[(int) x][h + 1][(int) z] = new Block(Block.BlockType.BlockType_Water);
-                        Blocks[(int) x][h + 2][(int) z] = new Block(Block.BlockType.BlockType_Water);
-
-                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH),
-                                (float) (y + 1 * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)),
-                                (float) (startZ + z * CUBE_LENGTH)));
-                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y + 1][(int) z])));
-                        VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int) (x)][(int) (y + 1)][(int) (z)]));
-
-                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH),
-                                (float) (y + 2 * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)),
-                                (float) (startZ + z * CUBE_LENGTH)));
-                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y + 2][(int) z])));
-                        VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int) (x)][(int) (y + 2)][(int) (z)]));
-
-                        //Generate Sand around water
-                        if (x > 0 && Blocks[(int)x][(int)y+2][(int)z].GetID() == 2) {
-                            if (Blocks[(int) x - 1][(int)y + 2][(int) z].GetID() != 2) {
-                                Blocks[(int) x - 1][(int)y + 1][(int) z] = new Block(Block.BlockType.BlockType_Sand);
-                                Blocks[(int) x - 1][(int)y + 2][(int) z] = new Block(Block.BlockType.BlockType_Sand);
-
-                            }
-                        }
-
-                        if (x < CHUNK_SIZE - 1 && Blocks[(int)x][(int)y+2][(int)z].GetID() == 2) {
-                            if (Blocks[(int) x + 1][(int)y + 2][(int) z].GetID() != 2) {
-                                Blocks[(int) x + 1][(int)y + 1][(int) z] = new Block(Block.BlockType.BlockType_Sand);
-                                Blocks[(int) x + 1][(int)y + 2][(int) z] = new Block(Block.BlockType.BlockType_Sand);
-
-                            }
-                        }
-                        if (z > 0 && Blocks[(int)x][(int)y+2][(int)z].GetID() == 2) {
-                            if (Blocks[(int) x][(int)y + 2][(int) z - 1].GetID() != 2) {
-                                Blocks[(int) x][(int)y + 1][(int) z - 1] = new Block(Block.BlockType.BlockType_Sand);
-                                Blocks[(int) x][(int)y + 2][(int) z - 1] = new Block(Block.BlockType.BlockType_Sand);
-
-                            }
-                        }
-                        if (z < CHUNK_SIZE - 1 && Blocks[(int)x][(int)y+2][(int)z].GetID() == 2) {
-                            if (Blocks[(int) x][(int)y + 2][(int) z + 1].GetID() != 2) {
-                                Blocks[(int) x][(int)y + 1][(int) z + 1] = new Block(Block.BlockType.BlockType_Sand);
-                                Blocks[(int) x][(int)y + 2][(int) z + 1] = new Block(Block.BlockType.BlockType_Sand);
-
-                            }
-                        }
-                    }
 
                     //Generate Grass on general top
                     if (y == h && y != 0) {
                         Blocks[(int) x][h][(int) z] = new Block(Block.BlockType.BlockType_Grass);
+                        Blocks[(int) x][(int) y][(int) z].SetActive(true);
+
+                        //Generate Sand
+                        if (y == 2) {
+                            Blocks[(int) x][2][(int) z] = new Block(Block.BlockType.BlockType_Sand);
+                            Blocks[(int) x][(int) y][(int) z].SetActive(true);
+                        }
                     }
 
                     VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH),
@@ -120,6 +79,22 @@ public class Chunk {
                             (float) (startZ + z * CUBE_LENGTH)));
                     VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
                     VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int) (x)][(int) (y)][(int) (z)]));
+                }
+
+                //Generate water
+                if (!Blocks[(int) x][1][(int) z].IsActive()) {
+
+                    for (int i = 1; i < 3; i++) {
+                        Blocks[(int) x][i][(int) z] = new Block(Block.BlockType.BlockType_Water);
+
+                        Blocks[(int) x][i][(int) z].SetActive(true);
+                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH),
+                                (float) (i * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)),
+                                (float) (startZ + z * CUBE_LENGTH)));
+                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][i][(int) z])));
+                        VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int) (x)][i][(int) (z)]));
+
+                    }
                 }
             }
         }
@@ -199,35 +174,14 @@ public class Chunk {
             for (int y = 0; y < CHUNK_SIZE; y++) {
                 for (int z = 0; z < CHUNK_SIZE; z++) {
 
-//                     if(y == 3){
-//                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Grass);
-//                        
-//                    }else
                     if (y == 0) {
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Bedrock);
-                    } //                    else if (y < 3){
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);
-                    //
-                    //                    }
-                    //                    else if (r.nextFloat() > 0.7f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Grass);
-                    //                    } else if (r.nextFloat() > 0.6f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Wood);   
-                    //                   
-                    //                    } else if (r.nextFloat() > 0.5f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Sand);
-                    //                    } else if (r.nextFloat() > 0.3f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Water);
-                    //                    } else if (r.nextFloat() > 0.2f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Stone);
-                    //                    } else if (r.nextFloat() > 0.1f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);    
-                    //                    } else if (r.nextFloat() > 0.0f) {
-                    //                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Bedrock);
-                    //
-                    //                        //Need default block
-                    //                    }
-                    else {
+                        Blocks[x][y][z].SetActive(true);
+                    } else if (r.nextFloat() > 0.1f) {
+                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);
+                    } else if (r.nextFloat() > 0.0f) {
+                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Stone);
+                    } else {
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);
                     }
                 }
@@ -242,20 +196,6 @@ public class Chunk {
 
         rebuildMesh(startX, startY, startZ);
 
-//        for(int i = 0; i < CHUNK_SIZE; i++){
-//            for(int j = 0; j < 1; j++){
-//                for(int k = 0; k < CHUNK_SIZE; k++){
-//                    
-//                    if(Blocks[i][j][k].GetID() == 5){
-//                                            System.out.println(Blocks[i][j+1][k].GetID());
-//                        if(Blocks[i][j+1][k].GetID() != 3 && Blocks[i][j+1][k].GetID() != 0){
-//                            Blocks[i][j+1][k] = new Block(Block.BlockType.BlockType_Water);
-//                        }
-//                    }
-//                    //System.out.println(Blocks[i][j][k].GetID());
-//                }
-//            }
-//        }
     }
 
     public static float[] createTexCube(float x, float y, Block block) {
@@ -331,35 +271,35 @@ public class Chunk {
             case 2: //Water
                 return new float[]{
                     // BOTTOM QUAD(DOWN=+Y)
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0,
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12,
                     // TOP
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0,
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12,
                     // FRONT QUAD
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0,
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12,
                     // BACK QUAD
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0,
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12,
                     // LEFT QUAD
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0,
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12,
                     // RIGHT QUAD
-                    x + offset * 15, y + offset * 1,
-                    x + offset * 14, y + offset * 1,
-                    x + offset * 14, y + offset * 0,
-                    x + offset * 15, y + offset * 0};
+                    x + offset * 15, y + offset * 13,
+                    x + offset * 14, y + offset * 13,
+                    x + offset * 14, y + offset * 12,
+                    x + offset * 15, y + offset * 12};
             case 3: //Dirt
                 return new float[]{
                     // BOTTOM QUAD(DOWN=+Y)
